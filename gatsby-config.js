0da2c8e94e843b4bd41ba4f34fdc51856c20fc8a
Antonio Llamas <antoniollamasln@gmail.com>
2024-01-11 09:51:37 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,43 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
-  plugins: [],
+	siteMetadata: {
+		title: 'The Bridge - Conceptos Full Stack (prework)',
+	},
+	trailingSlash: 'never',
+	plugins: [
+		`gatsby-plugin-layout`,
+		'gatsby-plugin-emotion',
+		'gatsby-plugin-sharp',
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/concepts`,
+				name: 'markdown-pages',
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 800,
+						},
+					},
+					{
+						resolve: `gatsby-remark-interactive-gifs`,
+						options: {
+							root: `${__dirname}`,
+							src: `${__dirname}/concepts/images`,
+							dest: `${__dirname}/public/static/gifs`,
+							play: `${__dirname}/concepts/images/play.png`,
+							placeholder: `${__dirname}/concepts/images/placeholder.gif`,
+							loading: `${__dirname}/concepts/images/loading.gif`,
+							relativePath: `/static/gifs`,
+						},
+					},
+				],
+			},
+		},
+	],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,51 @@
-import * as React from "react"
+import * as React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+import ConceptList from '../components/ConceptList'
+
+import { useStaticQuery, graphql } from 'gatsby'
+
+const IndexPage = () => {
+	const data = useStaticQuery(
+		graphql`
+			query HomepageTOC {
+				site {
+					siteMetadata {
+						title
+					}
+				}
+				allMarkdownRemark(sort: { order: ASC, fields: [frontmatter___order] }) {
+					edges {
+						node {
+							id
+							frontmatter {
+								order
+								path
+								title
+							}
+						}
+					}
+				}
+			}
+		`
+	)
+	console.log(data)
+	return (
+		<ConceptList
+			title="Lista de conceptos"
+			content={data.allMarkdownRemark.edges}
+		/>
+	)
 }
+
+export default IndexPage
+
+// export function Head() {
+// 	return (
+// 		<>
+// 			<link
+// 				href="https://fonts.cdnfonts.com/css/futura-std-4"
+// 				rel="stylesheet"
+// 			></link>
+// 		</>
+// 	)
+// }
